36eb65c94304c25b2ade035fbc8a2128287f8c90
val <130545294+val8elster@users.noreply.github.com>
2024-04-04 14:08:41 +0200

    added test text file

---
+++ DB/actions2.sql
@@ -0,0 +1,11 @@
+SELECT first_question();
+
+SELECT answer_question(1, 1, 1, 1);
+SELECT answer_question(2, 2, 1, 1);
+SELECT answer_question(2, 1, 1, 1);
+SELECT answer_question(3, 4, 9, 1);
+SELECT answer_question(4, 2, 3, 1);
+SELECT answer_question(7, 3, 2, 1);
+
+
+SELECT decomp();

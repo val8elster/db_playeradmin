6670fe236962f7e4eecd42f8eebef7db051fc90b
val <130545294+val8elster@users.noreply.github.com>
2024-04-04 13:55:48 +0200

    dfghk

--- DB/actions.sql
+++ DB/actions.sql
@@ -14,7 +14,18 @@ VALUES
 	('Player12'),
 	('Player13'),
 	('Player14'),
-	('Player15');
+	('Player15'),
+	('Player16'),
+	('Player17'),
+	('Player18'),
+	('Player19'),
+	('Player20'),
+	('Player21'),
+	('Player22'),
+	('Player23'),
+	('Player24'),
+	('Player25'),
+	('Player26:tbdeleted');
 	
 
 
@@ -34,7 +45,19 @@ VALUES
 	(12),
 	(13), 
 	(14), 
-	(15);
+	(15),
+	(16),
+	(17),
+	(18),
+	(19),
+	(20),
+	(21),
+	(22),
+	(23),
+	(24),
+	(25),
+	(26);
+
 
 
 
@@ -46,15 +69,21 @@ INSERT INTO plays (playerId)
 VALUES 
 	(1), 
 	(2), 
-	(3);
+	(3),
+	(16),
+	(17),
+	(18);
 
 
 
-INSERT INTO teams (teamname, teamLeaderId, active)
+INSERT INTO teams (teamname, teamLeaderId, active, gameId)
 VALUES
-	('Team1', 1, B'1'),
-	('Team2', 2, B'1'),
-	('Team3', 3, B'1');
+	('Team1', 1, B'1', 1),
+	('Team2', 2, B'1', 1),
+	('Team3', 3, B'1', 1),
+	('Team4', 16, B'1', 2),
+	('Team5', 17, B'1', 2),
+	('Team6', 18, B'1', 2);
 
 
 
@@ -84,6 +113,42 @@ LIMIT 4;
 
 INSERT INTO plays (playerId, teamId, gameId)
 SELECT playerId, 3, 1
+FROM players
+WHERE NOT EXISTS (
+    SELECT playerId
+    FROM plays
+    WHERE plays.playerId = players.playerId
+)
+LIMIT 4;
+
+
+
+INSERT INTO plays (playerId, teamId, gameId)
+SELECT playerId, 4, 2
+FROM players
+WHERE NOT EXISTS (
+    SELECT playerId 
+    FROM plays
+    WHERE plays.playerId = players.playerId
+)
+LIMIT 4;
+
+
+
+INSERT INTO plays (playerId, teamId, gameId)
+SELECT playerId, 5, 2
+FROM players
+WHERE NOT EXISTS (
+    SELECT playerId 
+    FROM plays
+    WHERE plays.playerId = players.playerId
+)
+LIMIT 4;
+
+
+
+INSERT INTO plays (playerId, teamId, gameId)
+SELECT playerId, 6, 2
 FROM players
 WHERE NOT EXISTS (
     SELECT playerId
@@ -127,9 +192,3 @@ VALUES
 
 
 SELECT add_questions_to_session(1, 2, 3, 4, 7);
-
-
-
-SELECT change_playername(1, 'New1');
-SELECT change_teamname(1, 1, '1stTeamBestTeam');
-SELECT change_teamname(1, 2, 'wrongShit');
